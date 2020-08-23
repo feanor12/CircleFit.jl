7c79a9b5dcd0b2eda000ccdfdc6fd6462053c3b7
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2020-08-23 18:04:41 +0200

    add weights to the points

--- src/CircFit.jl
+++ src/CircFit.jl
@@ -14,4 +14,17 @@ function circfit(x::AbstractArray,y::AbstractArray,p0::AbstractArray,kwargs...)
     lmfit(f,p0,T[];kwargs...)
 end
 
+"""
+x: Array of x-coordinates
+y: Array of y-coordinates
+w: Array of weights
+p0: Array of initial values [center_x, center_y, radius] 
+"""
+function circfit(x::AbstractArray,y::AbstractArray,wt::AbstractArray,p0::AbstractArray,kwargs...)
+    u = sqrt.(wt)
+    f = (p) -> @. u*(p[3]^2 - (x-p[1])^2 - (y-p[2])^2)
+    lmfit(f,p0,wt;kwargs...)
+end
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -29,3 +29,25 @@ for _ in 1:10
     @assert y0 ≈ result.param[2]
     @assert r ≈ result.param[3]
 end
+
+
+# test weights
+for _ in 1:10
+    x0 = rand()
+    y0 = rand()
+    r = rand()
+    x = r.*[-1.0,0,0,1,sqrt(2)/2,sqrt(2)/2,0,0,0] .+ x0
+    y = r.*[0.0,1,-1,0,sqrt(2)/2,-sqrt(2)/2,1.1,1.1,1.1] .+ y0
+    p0 = [0.0,0,1]
+    result1 = circfit(x,y,p0)
+    x = r.*[-1.0,0,0,1,sqrt(2)/2,sqrt(2)/2,0] .+ x0
+    y = r.*[0.0,1,-1,0,sqrt(2)/2,-sqrt(2)/2,1.1] .+ y0
+    wt = [1,1,1,1,1,1,3]
+    result2 = circfit(x,y,wt,p0)
+    wt = wt ./ sum(wt)
+    result3 = circfit(x,y,wt,p0)
+
+    for i in 1:3
+        @assert result1.param[i] ≈ result2.param[i] ≈ result3.param[i]
+    end
+end
